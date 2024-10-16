bc3a7464f0a0f22bf2eef6bfe4eb1f2fe07bcea5
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-10-16 22:46:49 +0600

    create all apps for routes

---
+++ PH/M45/intro-react-router/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <>
+            <div>
+                <h1>
+                    Thi is Footer Page!!
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
---
+++ PH/M45/intro-react-router/src/components/Users/Users.jsx
@@ -0,0 +1,12 @@
+const Users = () => {
+    return (
+        <>
+            <div>
+                <h1>This users page.....</h1>
+            </div>
+        </>
+    )
+}
+
+
+export default Users;
