fa6e4ba1d82628f352d6e49fd297e58b1155b310
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-10-16 23:19:38 +0600

    explore react routes

--- PH/M45/intro-react-router/src/components/Home/Home.jsx
+++ PH/M45/intro-react-router/src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+
 const Home = () => {
 
     return (
         <>
             <div>
                 <h1>This is home page!!!</h1>
+                <Header></Header>
+                <Outlet></Outlet>
             </div>
         </>
     )
--- PH/M45/intro-react-router/src/main.jsx
+++ PH/M45/intro-react-router/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App.jsx'
 import './index.css'
+import Home from './components/Home/Home.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
+
+import Users from './components/Users/Users.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [{
+      path: "/contact",
+      element: <Contact></Contact>,
+    },
+    {
+      path: "/about",
+      element: <About></About>,
+    },
+    {
+      path: "/users",
+      element: <Users></Users>,
+    }
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
